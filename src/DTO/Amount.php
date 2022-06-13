c7555a90a3738809dd062771e6ed755f528d2f85
Уразов Дмитрий <da@131.ru>
2022-06-13 11:30:23 +0300

    Delete currency validation

--- src/DTO/Amount.php
+++ src/DTO/Amount.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Bank131\SDK\DTO;
 
-use Bank131\SDK\DTO\Enum\CurrencyEnum;
 use Bank131\SDK\Exception\InvalidArgumentException;
 
 class Amount
@@ -31,12 +30,6 @@ class Amount
             throw new InvalidArgumentException('Amount value must be greater than 0');
         }
 
-        if (!$this->isCurrencyValid($currency)) {
-            throw new InvalidArgumentException(
-                'Currency value must be one of: ' . implode(', ', CurrencyEnum::all())
-            );
-        }
-
         $this->amount   = $amount;
         $this->currency = $currency;
     }
@@ -55,24 +48,5 @@ class Amount
     public function getCurrency(): string
     {
         return $this->currency;
-    }
-
-    /**
-     * @psalm-suppress MixedAssignment
-     * @psalm-suppress MixedArgument
-     *
-     * @param string $currency
-     *
-     * @return bool
-     */
-    private function isCurrencyValid(string $currency): bool
-    {
-        foreach (CurrencyEnum::all() as $existentCurrency) {
-            if (strcasecmp($currency, $existentCurrency) === 0) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
