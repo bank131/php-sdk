99eef4cf9b10b3b6acc220035a5c22c717f0a520
vstolpovski <vstolpovski@131.ru>
2023-02-13 15:09:07 +0300

    PAHOM-2434 added TransactionSplitInfo constructor

--- src/DTO/TransactionSplit/TransactionSplitInfo.php
+++ src/DTO/TransactionSplit/TransactionSplitInfo.php
@@ -10,4 +10,12 @@ class TransactionSplitInfo
      * @var Recipient[]
      */
     private $revenue_split_info;
+
+    /**
+     * @param Recipient[] $revenue_split_info
+     */
+    public function __construct(array $revenue_split_info)
+    {
+        $this->revenue_split_info = $revenue_split_info;
+    }
 }
