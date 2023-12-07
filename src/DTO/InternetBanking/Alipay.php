cfe6f874e942de5ba56c76fa6b192326a5c27abc
vstolpovski <vstolpovski@smart-glocal.com>
2023-12-07 22:54:16 +0300

    PAHOM-3739 alipay

--- src/DTO/InternetBanking/Alipay.php
+++ src/DTO/InternetBanking/Alipay.php
@@ -6,23 +6,8 @@ namespace Bank131\SDK\DTO\InternetBanking;
 
 class Alipay extends AbstractInternetBanking
 {
-    /**
-     * @var string $device
-     */
-    private $device;
-
-    public function __construct(string $device)
-    {
-        $this->device = $device;
-    }
-
     public function getType(): string
     {
         return InternetBankingEnum::ALIPAY;
-    }
-
-    public function getDevice(): string
-    {
-        return $this->device;
     }
 }
