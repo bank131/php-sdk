3783e4586a8a3e63910755985eff97307383e387
Maksim Vesnin <mvesnin@131.ru>
2024-10-14 13:01:21 +0300

    DT-172 add new payment method

--- src/DTO/InternetBanking/InternetBankingEnum.php
+++ src/DTO/InternetBanking/InternetBankingEnum.php
@@ -6,6 +6,7 @@ namespace Bank131\SDK\DTO\InternetBanking;
 
 final class InternetBankingEnum
 {
-    public const SBER_PAY='sber_pay';
-    public const ALIPAY='alipay';
+    public const SBER_PAY = 'sber_pay';
+    public const ALIPAY = 'alipay';
+    public const WECHAT = 'wechat';
 }
---
+++ src/DTO/InternetBanking/WeChat.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Bank131\SDK\DTO\InternetBanking;
+
+class WeChat extends AbstractInternetBanking
+{
+    public function getType(): string
+    {
+        return InternetBankingEnum::ALIPAY;
+    }
+}
