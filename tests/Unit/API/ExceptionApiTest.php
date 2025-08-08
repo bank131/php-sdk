1477c9637c727329ee85c1c9f39e7f0cf1e65248
Alexandr Dikov <adikov@131.ru>
2025-08-08 10:15:28 +0300

    DT-1027: Add test

--- tests/Unit/API/ExceptionApiTest.php
+++ tests/Unit/API/ExceptionApiTest.php
@@ -6,6 +6,7 @@ namespace Bank131\SDK\Tests\Unit\API;
 
 use Bank131\SDK\API\Request\Builder\RequestBuilderFactory;
 use Bank131\SDK\Exception\ApiException;
+use Bank131\SDK\Exception\IdempotencyKeyAlreadyExistsException;
 use GuzzleHttp\Psr7\Response;
 
 class ExceptionApiTest extends AbstractApiTest
@@ -51,6 +52,30 @@ class ExceptionApiTest extends AbstractApiTest
         try {
             $client->session()->create($sessionRequest);
         } catch (ApiException $exception) {
+            $this->assertEquals($exceptionCode, $exception->getApiCode());
+        }
+    }
+
+    public function testIdempotencyKeyAlreadyExistException(): void
+    {
+        $expectedResponse = [
+            'error' => [
+                'code' => $exceptionCode = 'idempotency_key_already_exists',
+                'description' => 'already in progress or processed'
+            ]
+        ];
+
+        $client = $this->createClientWithMockResponse([
+            new Response(400, [], json_encode($expectedResponse))
+        ]);
+
+        $sessionRequest = RequestBuilderFactory::create()
+            ->createPaymentSession()
+            ->build();
+
+        try {
+            $client->session()->create($sessionRequest);
+        } catch (IdempotencyKeyAlreadyExistsException $exception) {
             $this->assertEquals($exceptionCode, $exception->getApiCode());
         }
     }
