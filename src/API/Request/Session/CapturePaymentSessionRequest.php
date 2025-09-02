d19d838e5b6bff68daddcf0c4cb3be933757767f
Веснин Максим <mvesnin@131.ru>
2025-09-02 14:45:13 +0300

    DT-1135

--- src/API/Request/Session/CapturePaymentSessionRequest.php
+++ src/API/Request/Session/CapturePaymentSessionRequest.php
@@ -3,26 +3,17 @@ declare(strict_types=1);
 
 namespace Bank131\SDK\API\Request\Session;
 
-use Bank131\SDK\API\Request\AbstractRequest;
 use Bank131\SDK\DTO\Amount;
 
-class CapturePaymentSessionRequest extends AbstractRequest
+class CapturePaymentSessionRequest extends AbstractSessionRequest
 {
-    /**
-     * @var string
-     */
-    private $session_id;
-
-    /**
-     * @var Amount
-     */
-    private $amount_details;
-
     public function __construct(
         string $sessionId,
         ?Amount $amount = null
     ) {
-        $this->session_id = $sessionId;
-        $this->amount_details = $amount;
+        $this->setSessionId($sessionId);
+        if ($amount !== null) {
+            $this->setAmount($amount);
+        }
     }
 }
