5ce79baf4fa6ba53cf9f47f6b2486cc2ede511b4
Alexandr Dikov <adikov@131.ru>
2026-09-08 15:43:56 +0300

    DT-2421: Add SberPayWidget inform

--- src/DTO/CustomerInteraction/Inform/Inform.php
+++ src/DTO/CustomerInteraction/Inform/Inform.php
@@ -7,15 +7,32 @@ namespace Bank131\SDK\DTO\CustomerInteraction\Inform;
 class Inform
 {
     /**
+     * @var string
+     */
+    private $type;
+
+    /**
      * @var QRInform|null
      */
     private $qr;
 
     /**
-     * @return QRInform|null
+     * @var Notification|null
      */
+    private $notification;
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
     public function getQr(): ?QRInform
     {
         return $this->qr;
+    }
+
+    public function getNotification(): ?Notification
+    {
+        return $this->notification;
     }
 }
